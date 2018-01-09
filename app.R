3db37b8a86a52516293f34d96a94718e95d3aecf
Hugo García <hugoallangm@gmail.com>
2018-01-09 17:13:00 -0600

    Avances con Entidad y opciones

--- app.R
+++ app.R
@@ -16,9 +16,10 @@ library(ckanr)
 
 ckanr_setup(url = "https://datos.minfin.gob.gt/")
 id_ejecucion <- "3635b9c2-5f0e-43ec-b3ce-4a006e029c57"
+id_ejecucion2016 <- "c1043a6e-a4af-478a-b06e-ebc2eb0e9a3"
 sql <- paste0('SELECT DISTINCT "Tipo Gobierno","Entidad"   from \"', id_ejecucion, '"\  WHERE "Tipo Gobierno"=\'ADMINISTRACIÓN CENTRAL\'  ')
 categorias <- ds_search_sql(sql, as = 'table')
-categorias <- categorias$records$Entidad
+categorias <- as.list( categorias$records$Entidad )
 
 
 # datos <- as.data.table( read.xlsx('/mnt/Datos/GitHub/Transparencia/ejecucion_mensualizada_2017.xlsx', sheet = 1) )
@@ -35,9 +36,40 @@ ui <- fluidPage(
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
-    selectInput("Entidad", h4("Seleccione la entidad"),
-                choices = categorias
+     
+     sidebarPanel(
+       selectInput("Entidad", h4("Seleccione la entidad"),
+                   choices = categorias,
+                   selected = NULL
+       ),
+       
+
+      selectInput("Opcion", h4("¿Qué desea consultar de esta entidad?"),
+                     choices = c("Unidad ejecutora" = "1",  "Programa" = "2", "Ejecución por mes" = "3" )
       ),
+      
+      conditionalPanel(
+        condition = "input.Opcion == '1' ",
+        actionButton('btAccion','Ver las unidades ')
+      ),
+      
+      conditionalPanel(
+        condition = "input.Opcion == '2' ",
+        actionButton('btAccion','Ver los Programas ')
+      ),
+      
+      conditionalPanel(
+        condition = "input.Opcion == '3' ",
+        actionButton('btAccion','Ver los meses disponibles ')
+      ),
+      
+      uiOutput(outputId = "Opciones")
+      
+     ),
+
+
+
+     
       
       # Show a plot of the generated distribution
       mainPanel(
@@ -49,6 +81,28 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
+  ## keep track of elements inserted and not yet removed
+  inserted <- c()
+  
+  observeEvent(
+    input$btAccion, {
+      btn <- input$btAccion
+      id <- paste0('txt', btn)
+      label_opciones <- ""
+      if(input$Opcion == 1 )
+        label_opciones = "Selecciones las unidades ejecutoras:"
+      else if( input$Opcion == 2 )
+        label_opciones = "Selecciones los programas:"
+      output$Opciones <- renderUI({
+        selectInput("OpcionesLista",label_opciones, c("j"))
+      })
+    }
+  )
+  
+  # consulta_sql = paste0('select distinct "Programa", "Entidad" from "',
+  #                       id_ejecucion, '" where "Entidad" = \'', input$Entidad, "\'")
+  # resultado <- ds_search_sql(consulta_sql, as = 'table')
+  
    output$distPlot <- renderPlot({
       
    })
